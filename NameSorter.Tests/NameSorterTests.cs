ca16ff0979612251d17ffdecaf7e754b58e6523e
Shah-Hemal <shemalhah@gmail.com>
2025-04-25 01:15:05 -0400

    Add NameSorter.Tests

---
+++ NameSorter.Tests/NameSorterTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NameSorter;
+using Moq;
+
+namespace NameSorter.Tests
+{
+    [TestFixture]
+    public class NameSorterTests
+    {
+
+        [Test]
+        public void SortNamesFromFile_FileNotFound_ThrowsFileNotFoundException()
+        {
+            var sorter = new NameSortingService();
+            Assert.Throws<FileNotFoundException>(() => sorter.SortNamesFromFile("non-existent-file.txt"));
+        }
+
+        [Test]
+        public void SortNamesFromFile_ValidFile_ReturnsSortedNames()
+        {
+            var unsortedNames = new[] { "John Smith", "Jane Smith", "Peter Smith" };
+            File.WriteAllLines("test-unsorted-names.txt", unsortedNames);
+            var sorter = new NameSortingService();
+
+            var sortedNames = sorter.SortNamesFromFile("test-unsorted-names.txt");
+            File.Delete("test-unsorted-names.txt");
+
+            Assert.That(sortedNames, Is.EqualTo(new List<string> { "Jane Smith", "John Smith", "Peter Smith" }));
+        }
+
+        [Test]
+        public void SortNamesFromFile_InvalidNameFormat_ThrowsArgumentException()
+        {
+            var invalidNames = new[] { "SingleName" };
+            File.WriteAllLines("test-invalid-names.txt", invalidNames);
+            var sorter = new NameSortingService();
+            Assert.Throws<ArgumentException>(() => sorter.SortNamesFromFile("test-invalid-names.txt"));
+            File.Delete("test-invalid-names.txt");
+        }
+
+        [Test]
+        public void PrintNamesToConsole_ValidList_PrintsEveryName()
+        {
+            var namesToPrint = new List<string> { "Demo Name1", "Demo Name2" };
+            var mockConsole = new Mock<IConsoleWrapper>();
+            var sorter = new NameSortingService();
+            sorter.ConsoleWrapper = mockConsole.Object; // Directly setting property
+
+            sorter.PrintNamesToConsole(namesToPrint);
+
+            mockConsole.Verify(c => c.WriteLine("Demo Name1"), Times.Once);
+            mockConsole.Verify(c => c.WriteLine("Demo Name2"), Times.Once);
+        }
+
+        [Test]
+        public void WriteSortedNamesListToFile_ValidList_WritesToFile()
+        {
+            var sortedNames = new List<string> { "Justin Trudeau", "Donald Trump" };
+            var sorter = new NameSortingService();
+            string outputFilePath = "sorted-names-list.txt"; // Provide the output file path
+
+            sorter.WriteSortedNamesToFile(sortedNames, outputFilePath); // Pass the outputFilePath
+          
+            Assert.IsTrue(File.Exists(outputFilePath));
+            var fileContent = File.ReadAllLines(outputFilePath);
+            Assert.That(fileContent, Is.EqualTo(sortedNames));
+
+            File.Delete(outputFilePath);
+        }
+    }
+}
---
+++ NameSorter.Tests/PersonNameTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NameSorter;
+
+namespace NameSorter.Tests
+{
+    [TestFixture]
+    public class PersonNameTests
+    {
+        [Test]
+        public void PersonName_NullOrWhitespaceFullName_ThrowsArgumentNullException()
+        {
+            // By putting Pragma ignoring the overcautious compiler check-warning on passing null 
+            #nullable disable
+            Assert.Throws<ArgumentNullException>(() => new PersonName(null));
+            #nullable restore
+            Assert.Throws<ArgumentNullException>(() => new PersonName(" "));
+        }
+
+        [Test]
+        public void PersonName_ValidFullName_SetsPropertiesCorrectly()
+        {
+            var name = new PersonName("Hemal Shah");
+            Assert.That(name.GivenNames, Is.EqualTo(new List<string> { "Hemal" }));
+            Assert.That(name.LastName, Is.EqualTo("Shah"));
+
+            name = new PersonName("Hemal Girishkumar Shah");
+            Assert.That(name.GivenNames, Is.EqualTo(new List<string> { "Hemal", "Girishkumar" }));
+            Assert.That(name.LastName, Is.EqualTo("Shah"));
+        }
+
+        [Test]
+        public void PersonName_UpTo_Three_GivenNames_Accepted()
+        {
+            Assert.DoesNotThrow(() => new PersonName("Given1 Given2 Given3 LastName"));
+
+            Assert.Throws<ArgumentException>(() => new PersonName("Given1 Given2 Given3 Given4 LastName"));
+            Assert.Throws<ArgumentException>(() => new PersonName(" Given1 Given2 Given3 Given4 LastName")); // Leading space
+            Assert.Throws<ArgumentException>(() => new PersonName("Given1 Given2 Given3 Given4  LastName")); // Multiple spaces
+            Assert.Throws<ArgumentException>(() => new PersonName("Given1 Given2 Given3 Given4 LastName ")); // Trailing space
+        }
+
+        [Test]
+        public void PersonName_FullNameWithLeadingOrTrailingSpaces_HandlesCorrectly()
+        {
+            var name = new PersonName("  Hemal Shah ");
+            Assert.That(name.GivenNames, Is.EqualTo(new List<string> { "Hemal" }));
+            Assert.That(name.LastName, Is.EqualTo("Shah"));
+        }
+
+        [Test]
+        public void PersonName_SingleName_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new PersonName("Hemal"));
+        }
+
+        [Test]
+        public void PersonName_CompareTo_SortsByLastNameThenGivenNames()
+        {
+            var name1 = new PersonName("Janet Parsons");
+            var name2 = new PersonName("Vaughn Lewis");
+            var name3 = new PersonName("Marin Alvarez");
+            var name4 = new PersonName("Marin Beatty");
+            var name5 = new PersonName("Marin Alvarez Bravo");
+
+            Assert.Less(name3.CompareTo(name1), 0); // Alvarez < Parsons
+            Assert.Greater(name2.CompareTo(name3), 0); // Lewis > Alvarez
+            Assert.Less(name3.CompareTo(name4), 0); // Alvarez < Beatty
+            Assert.Less(name3.CompareTo(name5), 0); // Alvarez < Alvarez Bravo
+            Assert.Greater(name5.CompareTo(name3), 0); // Alvarez Bravo > Alvarez
+        }
+    }
+}
